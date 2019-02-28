89294e4d8bc469aa6df643605e6059fd1e14b295
lmyEsther <esthercheremo@gmail.com>
2019-02-28 18:50:11 -0500

    funcion y test de extractAllRoutes

--- tests/convertir-rutaAbsoluta.spec.js
+++ tests/convertir-rutaAbsoluta.spec.js
@@ -1,13 +1,13 @@
 import { toAbsolute } from '../lib/utils/convertir-RutaAbsoluta.js';
 
 describe('toAbsolute', () => {
-    it('Debería ser una función', () => {
-        expect(typeof toAbsolute).toBe('function');
-    });
-    it('Debería regresar una ruta absoluta si se le ingreso una ruta relativa', () => {
-        expect(toAbsolute('tests\\test-imp\\archivo2.txt')).toBe('C:\\Users\\Betech\\Desktop\\Laboratoria\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt')
-    });
-    it('Debería regresar la misma ruta si es absoluta', () => {
-        expect(toAbsolute('C:\\Users\\Betech\\Desktop\\Laboratoria\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt')).toBe('C:\\Users\\Betech\\Desktop\\Laboratoria\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt')
-    });
+  it('Debería ser una función', () => {
+    expect(typeof toAbsolute).toBe('function');
+  });
+  it('Debería regresar una ruta absoluta si se le ingreso una ruta relativa', () => {
+    expect(toAbsolute('tests\\test-imp\\archivo2.txt')).toBe('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt');
+  });
+  it('Debería regresar la misma ruta si es absoluta', () => {
+    expect(toAbsolute('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt')).toBe('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt');
+  });
 });
--- tests/extraer-md.spec.js
+++ tests/extraer-md.spec.js
@@ -1,15 +1,24 @@
+import { extractAllRoutes } from '../lib/utils/extraer-md.js';
 
-/* describe('extractAllRoutes',() => {
-    it('debería ser una función', () => {
-        expect(typeof extractAllRoutes).toBe('function');
-    });
-    it('debería recorrer una carpeta y devolver todas las rutas de los archivos en array de string', () => {
-        const pathsArr = extractAllRoutes('');
-        expect(pathsArr.length).toBe();
-    });
-    it('debería recorrer una carpeta y devolver todas las rutas de los archivos', () => {
-        const pathsArr = extractAllRoutes('');
-        expect(pathsArr.length).toBe();
-        expect(pathsArr.includes('')).toBe(true);
-    });
-}); */
+describe('extractAllRoutes', () => {
+  it('debería ser una función', () => {
+    expect(typeof extractAllRoutes).toBe('function');
+  });
+  it('es archivo', () => {
+    expect(extractAllRoutes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt')).toEqual(['C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt']);
+  });
+  it('no es archivo', () => {
+    expect(extractAllRoutes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp'))
+      .toEqual(['C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp//archivo2.txt', 'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp//archivo3.js',
+        'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp//test-1//archivo.md']);
+  });
+  // it('debería recorrer una carpeta y devolver todas las rutas de los archivos en array de string', () => {
+  //     const pathsArr = extractAllRoutes('');
+  //     expect(pathsArr.length).toBe();
+  // });
+  // it('debería recorrer una carpeta y devolver todas las rutas de los archivos', () => {
+  //     const pathsArr = extractAllRoutes('');
+  //     expect(pathsArr.length).toBe();
+  //     expect(pathsArr.includes('')).toBe(true);
+  // });
+});
