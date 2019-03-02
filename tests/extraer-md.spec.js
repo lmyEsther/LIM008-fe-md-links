88612bf5ad00b4f47301b9d4b7aacbb1d4877048
lmyEsther <esthercheremo@gmail.com>
2019-03-01 19:21:28 -0500

    funcion filterMD testeada, inicio leer archivo

--- tests/extraer-md.spec.js
+++ tests/extraer-md.spec.js
@@ -1,4 +1,4 @@
-import { extractAllRoutes } from '../lib/utils/extraer-md.js';
+import { extractAllRoutes, filterMD } from '../lib/utils/extraer-md.js';
 
 describe('extractAllRoutes', () => {
   it('debería ser una función', () => {
@@ -7,18 +7,23 @@ describe('extractAllRoutes', () => {
   it('es archivo', () => {
     expect(extractAllRoutes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt')).toEqual(['C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt']);
   });
-  it('no es archivo', () => {
+  it('es directorio', () => {
     expect(extractAllRoutes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp'))
-      .toEqual(['C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp//archivo2.txt', 'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp//archivo3.js',
-        'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp//test-1//archivo.md']);
+      .toEqual(['C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt', 'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo3.js',
+        'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\test-1\\archivo.md', 'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\test-1\\archivo4.md']);
   });
-  // it('debería recorrer una carpeta y devolver todas las rutas de los archivos en array de string', () => {
-  //     const pathsArr = extractAllRoutes('');
-  //     expect(pathsArr.length).toBe();
-  // });
-  // it('debería recorrer una carpeta y devolver todas las rutas de los archivos', () => {
-  //     const pathsArr = extractAllRoutes('');
-  //     expect(pathsArr.length).toBe();
-  //     expect(pathsArr.includes('')).toBe(true);
-  // });
+  it('debería recorrer una carpeta y devolver todas las rutas de los archivos en array de string', () => {
+    expect(extractAllRoutes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp').length).toBe(4);
+  });
+  it('debería recorrer una carpeta y devolver todas las rutas de los archivos', () => {
+    const arrayRoutes = extractAllRoutes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp');
+    expect(arrayRoutes.includes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo2.txt')).toBe(true);
+    expect(arrayRoutes.includes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\archivo3.js')).toBe(true);
+    expect(arrayRoutes.includes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\test-1\\archivo.md')).toBe(true);
+    expect(arrayRoutes.includes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\test-1\\archivo4.md')).toBe(true);
+  });
+  it('Debería devolver un array de archivos en formato md solamente', () => {
+    expect(filterMD(extractAllRoutes('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp'))).toEqual(['C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\test-1\\archivo.md',
+      'C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\tests\\test-imp\\test-1\\archivo4.md']);
+  });
 });
